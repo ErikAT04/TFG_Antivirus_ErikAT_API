656f64ab814c9e62e79478a29e6ff72fc489b417
Erik Amo <aterik.dev@gmail.com>
2025-01-23 17:43:38 +0100

    routers añadidos

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 import uvicorn
 from app.routers import SignatureRouter
+from app.routers import UserRouter
+from app.routers import DeviceRouter
 
 app = FastAPI()
 app.include_router(SignatureRouter.router)
+app.include_router(UserRouter.router)
+app.include_router(DeviceRouter.router)
 
 if __name__=="__main__":
     uvicorn.run("main:app", port=8000)
