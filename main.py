51f3f567f6ab170cc8641a037bf69d99b5513965
ErikAT <aterik.dev@gmail.com>
2025-01-26 14:36:42 +0100

    Prueba de Put

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ app.include_router(UserRouter.router)
 app.include_router(DeviceRouter.router)
 
 if __name__=="__main__":
-    uvicorn.run("main:app", port=8000)
+    uvicorn.run("main:app", port=8000, host="0.0.0.0")
